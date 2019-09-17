c9da5a8f441111daac7a5cf73bf9884aecf60620
Leonardo Hax Damiani <lhdamiani@gmail.com>
2019-09-17 17:23:13 +0200

    Removal of unused exception catch variables.

--- gitutils/gitutils_exception.py
+++ gitutils/gitutils_exception.py
@@ -5,10 +5,6 @@ import inspect
 
 class NoTraceBackWithLineNumber(Exception):
     def __init__(self, msg):
-        try:
-            ln = sys.exc_info()[-1].tb_lineno
-        except AttributeError:
-            ln = inspect.currentframe().f_back.f_lineno
         self.args = "{0.__name__} : {1}".format(type(self), msg),
         sys.exit(self)
 

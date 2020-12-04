c5627c424c860ea3d668ac9e8d012783b8227b1a
Leonardo Hax Damiani <leonardo.hax@psi.ch>
2020-12-04 13:20:17 +0100

    Feedback msgs for tests.

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#activates env
 source activate
 conda env create -f environment_droplet.yml
-
 conda activate gitutils 
 cd ${RUN_DIR}
 # decript the token file
@@ -10,6 +8,19 @@ echo "Please, type the password to decrypt the token file: "
 openssl aes-256-cbc -d -a -in /root/token.enc -out /root/.gitutils_token
 # run unit test
 echo "Run gitutils tests..."
-cd ./gitutils && python -m unittest gitutils.tests.gitutils_unittest
-# python -m unittest gitutils.tests.gitutils_cmds
+cd ./gitutils
+echo "Unit tests..."
+python -m unittest gitutils.tests.gitutils_unittest
+if [ $? = 0 ]; then
+    echo "Unit tests... Success!"
+else
+    echo "PROBLEM WITH UNIT TESTS"
+fi
+echo "Commands tests..."
+python -m unittest gitutils.tests.gitutils_cmds
+if [ $? = 0 ]; then
+    echo "Commands tests... Success!"
+else
+    echo "PROBLEM WITH UNIT TESTS"
+fi
 echo "Done."

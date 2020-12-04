9c3b5b44871e2dcdcd42517e27cdeece0d4e24a2
Leonardo Hax Damiani <leonardo.hax@psi.ch>
2020-12-04 13:53:49 +0100

    updated docker and startup script with utf8 and feedback msg

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-source activate
-conda env create -f environment_droplet.yml
-conda activate gitutils 
-cd ${RUN_DIR}
+LC_CTYPE=en_US.utf8
 # decript the token file
 echo "Please, type the password to decrypt the token file: "
 openssl aes-256-cbc -d -a -in /root/token.enc -out /root/.gitutils_token
+source activate
+conda env create -f environment_droplet.yml -n gitutils
+source activate gitutils
+cd ${RUN_DIR}
 # run unit test
 echo "Run gitutils tests..."
 cd ./gitutils
@@ -21,6 +22,6 @@ python -m unittest gitutils.tests.gitutils_cmds
 if [ $? = 0 ]; then
     echo "Commands tests... Success!"
 else
-    echo "PROBLEM WITH UNIT TESTS"
+    echo "PROBLEM WITH COMMAND TESTS..."
 fi
 echo "Done."

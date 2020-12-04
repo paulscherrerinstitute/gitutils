f10728bf2c3eb8792af8f7f512de93d4f4730e5b
Leonardo Hax Damiani <leonardo.hax@psi.ch>
2020-12-04 15:48:53 +0100

    -b to supress unittest output

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,19 +6,18 @@ openssl aes-256-cbc -d -a -in /root/token.enc -out /root/.gitutils_token
 source activate
 conda env create -f environment_droplet.yml -n gitutils
 source activate gitutils
-cd ${RUN_DIR}
+cd ${RUN_DIR}/gitutils
 # run unit test
 echo "Run gitutils tests..."
-cd ./gitutils
 echo "Unit tests..."
-python -m unittest gitutils.tests.gitutils_unittest
+python -m unittest gitutils.tests.gitutils_unittest -b
 if [ $? = 0 ]; then
     echo "Unit tests... Success!"
 else
     echo "PROBLEM WITH UNIT TESTS"
 fi
 echo "Commands tests..."
-python -m unittest gitutils.tests.gitutils_cmds
+python -m unittest gitutils.tests.gitutils_cmds -b
 if [ $? = 0 ]; then
     echo "Commands tests... Success!"
 else

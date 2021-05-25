9db7856f90b119bec0c2fc446d509f2f49186f94
lhdamiani <lhdamiani@gmail.com>
2021-05-25 15:34:35 +0200

    minimum pattern length.

--- gitutils/cmds/clonegroup.py
+++ gitutils/cmds/clonegroup.py
@@ -12,15 +12,26 @@ def clone_group(group_name='', pattern=None):
     projects from the specified group.
     : param group_name : Name of group to be cloned
     : type group_name : str
+    : param pattern : Filter of repository patterns (3 characters)
+    : type group_name : str
     """
     # check if group exists
     gitlab_utils.check_group_exists(group_name)
     # Gets all the projects from the group
-    projects = gitlab_utils.get_group_projects(group_name)
-    # Filter name pattern
-    if pattern:
-        projects = gitlab_utils.filter_name_pattern(projects, pattern)
-
+    if not pattern:
+        projects = gitlab_utils.get_group_projects(group_name)
+    else:
+        projects = []
+        for pat in pattern:
+            if len(pat) <= 2:
+                print(const.CLONEGROUP_PATTERN_TOO_SHORT % pat)
+            else:
+                projs = gitlab_utils.get_group_projects(group_name, pat)
+                for p in projs:
+                    projects.append(p)
+        if not projects:
+            print(const.CLONEGROUP_EMPTY)
+            return
     # clones all the projects from group
     print(const.CLONEGROUP_WARNING)
     for i in projects:
@@ -30,5 +41,7 @@ def clone_group(group_name='', pattern=None):
         # Gitlab API refuses if there's no sleep in between (too many requests)
         # error: ssh_exchange_identification: read: Connection reset by peer
         time.sleep(2)
+    msg = const.CLONEGROUP_FINISH
     # Finishing up, message to user
-    print(const.CLONEGROUP_FINISH)
+    print(msg)
+    return
